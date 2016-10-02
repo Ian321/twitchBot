fc64587ad13bc1db3ac2a49ab3a3ebefe86f32be
Ian321 <ignaz.k@live.de>
2016-10-02 04:38:21 +0200

    First Commit

---
+++ lib.js
@@ -0,0 +1,47 @@
+/*
+  Custom library
+*/
+module.exports = {
+  msToTimeSting: function (ms) {
+    sec = Math.floor(ms / 1000);
+    days = Math.floor(sec / 86400);
+		hours= Math.floor(sec / 3600 % 60 % 60 % 24);
+		mins = Math.floor(sec / 60 % 60);
+		secs = Math.floor(sec % 60);
+		time = "";
+		if (days >= 2) {
+			time += days+" days";
+		} else if (days >= 1) {
+			time += days+" day";
+		}
+		if (days >= 1 && ((hours >= 1 && mins >= 1) || (hours >= 1 && secs >= 1) || (mins >= 1 && secs >= 1))) {
+			time += ", ";
+		} else if (days >= 1 && (hours >= 1 || mins >= 1 || secs >= 1)) {
+			time += " and ";
+		}
+		if (hours >= 2) {
+			time += hours+" hours";
+		} else if (hours >= 1) {
+			time += hours+" hour";
+		}
+		if (hours >= 1 && mins >= 1 && secs >= 1) {
+			time += ", ";
+		} else if (hours >= 1 && (mins >= 1 || secs >= 1)) {
+			time += " and ";
+		}
+		if (mins >= 2) {
+			time += mins+" minutes";
+		} else if (mins >= 1) {
+			time += mins+" minute";
+		}
+		if (mins >= 1 && secs >= 1) {
+			time += " and ";
+		}
+		if (secs >= 2) {
+			time += secs+" seconds";
+		} else if (secs >= 1) {
+			time += secs+" second";
+		}
+		return time;
+  }
+};
---
+++ main.js
@@ -0,0 +1,92 @@
+/*
+  main.js
+  IRC Bot in node.js
+*/
+const tmi = require('tmi.js');
+const _ = require('lodash');
+
+const lib = require('./lib');
+const conf = require('././config.json');
+
+const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: conf.username,
+        password: conf.password
+    },
+    channels: ["#" + process.argv[2]]
+};
+
+const client = tmi.client(options);
+client.connect();
+const startTime = Date.now();
+const admins = conf.admins;
+var lastMS = "";
+
+const messages = [];
+
+setInterval(() => {
+    if (messages.length > 0) {
+        client.say(channel, message);
+    }
+}, 5000);
+
+
+const commands = {
+    '!say': say,
+    '*ping': ping,
+    '!list': list,
+    '!node': node
+};
+
+function sendMessage(channel, message) {
+    lastMS = message + " ";
+    return client.say(channel, message + " ").catch(err => console.log('Message not send.'));
+}
+
+client.on('chat', function(channel, user, message, self) {
+    if (self) return;
+
+    const match = message.match(/(.[a-zA-Z0-9_-]+)(?:\s+(.*))?/);
+    if (match !== null) {
+        const command = match[1].toLowerCase();
+        const args = match[2] ? match[2].trim().split(/\s+/) : [];
+
+        //console.log(command, args);
+        if (commands.hasOwnProperty(command)) {
+            channel = channel.substring(1);
+            if (admins.includes(user.username)) {
+                user.admin = true;
+            }
+            commands[command](channel, user, message, args);
+        }
+    }
+});
+
+/* Commands as function */
+function say(channel, user, message, args) {
+    if (user.admin) {
+        return sendMessage(channel, args.join(" "));
+    }
+}
+
+function ping(channel, user, message, args) {
+    return sendMessage(channel, "running for " + lib.msToTimeSting(Date.now() - startTime));
+}
+
+function list(channel, user, message, args) {
+    if (args[0] && args[0] !== '') {
+        channel = args[0];
+    }
+    client.whisper(user.username, `https://tmi.twitch.tv/group/user/${channel}/chatters`);
+}
+
+function node(channel, user, message, args) {
+    return sendMessage(channel, "i run on node js FeelsGoodMan");
+}
