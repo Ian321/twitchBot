31b32e3f38cf98a7ab8820d88ba3cf85876d9d76
Ian321 <ignaz.k@live.de>
2016-10-17 15:15:38 +0200

    Hotfix

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ function sha512(channel, user, message, args) {
 }
 
 function math(channel, user, message, args) {
-  var tmp = message.substr(message.indexOf(" ") + 1);
+  var tmp = message.substr(message.indexOf(" ") + 1).replace("\"", "");
   if (tmp.indexOf('import') > -1 || tmp.indexOf('range') > -1 || tmp.indexOf('eye') > -1 || tmp.indexOf('ones') > -1 || tmp.indexOf('tojson') > -1 || tmp.indexOf('topolar') > -1) {
     return sendMessage(channel, `${user.username}, that function is not allowed OMGScoots`);
   }
