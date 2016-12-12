0f0bf79486a29c61abb4b2da1ecb511bb7e69c10
Ian321 <ignaz.k@live.de>
2016-12-12 14:35:26 +0100

    Hotfix
    Fixed math again.

--- main.js
+++ main.js
@@ -115,93 +115,96 @@ function node(channel, user, message, args) {
 }
 
 function sha512(channel, user, message, args) {
-  return sendMessage(channel, require('crypto').createHash('sha512').update(args.join(" ")).digest('hex'));
+    return sendMessage(channel, require('crypto').createHash('sha512').update(args.join(" ")).digest('hex'));
 }
 
 function math(channel, user, message, args) {
-  // escape strings
-  var tmp = message.replace(/[\\$'"]/g, "\\$&");
-  tmp = tmp.substr(message.indexOf(" ") + 1).split("\"").join("");
-  if (tmp.indexOf('import') > -1 || tmp.indexOf('range') > -1 || tmp.indexOf('eye') > -1 ||
-  tmp.indexOf('ones') > -1 || tmp.indexOf('tojson') > -1 || tmp.indexOf('topolar') > -1 || tmp.indexOf('zeros') > -1 || tmp.indexOf('distance') > -1) {
-    return sendMessage(channel, `${user.username}, that function is not allowed OMGScoots`);
-  }
-  if (tmp.indexOf('isPrime') > -1) {
-      var tmpP = tmp.replace(/ /g, "").split("isPrime(")[1];
-      if (tmp.split("isPrime(")[2]) {
-        return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
-      } else if ((tmp.match(/\(/g) || []).length != (tmp.match(/\)/g) || []).length) {
-        return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
-      } else {
-        var count;
-        try {
-          count = (tmpP.match(/\(/g) || []).length;
-          count++;
-        } catch (e) {
-          return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
-        }
-        var rek = new RegExp("^(?:[^|\\)]*\\\)){"+count.toString()+"}([^|]*)", "gm");
-        var keepo = rek.exec(tmpP);
-        var pogchamp = keepo[0];
-        var vislaud = "";
-        keepo.shift();
-        for (var i = 0; i < keepo.length; i++) {
-          vislaud += keepo[i];
-        }
-        tmpP = "("+tmpP.replace(vislaud, "");
-        var resultE = false;
-        var resultP;
-        try {
-          resultP = mathjs.eval(tmpP);
-          if (!(resultP == "Infinity" || resultP == "-Infinity" || resultP.toString() == "NaN")) {
-            resultE = true;
-          }
-        } catch (e) {
-
-        }
-        if (!resultE) {
-          return sendMessage(channel, `${user.username}, can't check if it's a prime number WutFace`);
-        } else {
-          tmp = "isPrime("+resultP+")";
-        }
-      }
-  }
-  try {
-    var result = mathjs.eval(tmp);
-    if (result == "Infinity" || result == "-Infinity") {
-      result = `WutFace it's '${result}'`;
-    } else if (result.toString() == "NaN") {
-      result = `NaM it's '${result}'`;
+    // escape strings
+    var tmp = message.replace(/[\\$'"]/g, "\\$&");
+    tmp = tmp.substr(message.indexOf(" ") + 1).split("\"").join("");
+    if (tmp.indexOf('import') > -1 || tmp.indexOf('range') > -1 || tmp.indexOf('eye') > -1 ||
+        tmp.indexOf('ones') > -1 || tmp.indexOf('tojson') > -1 || tmp.indexOf('topolar') > -1 || tmp.indexOf('zeros') > -1 || tmp.indexOf('distance') > -1) {
+        return sendMessage(channel, `${user.username}, that function is not allowed OMGScoots`);
     }
-    return sendMessage(channel, `${user.username}, ${result}`);
-  } catch (e) {
-    return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
-  }
+    if (tmp.indexOf('isPrime') > -1) {
+        var tmpP = tmp.replace(/ /g, "").split("isPrime(")[1];
+        if (tmp.split("isPrime(")[2]) {
+            return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
+        } else if ((tmp.match(/\(/g) || []).length != (tmp.match(/\)/g) || []).length) {
+            return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
+        } else {
+            var count;
+            try {
+                count = (tmpP.match(/\(/g) || []).length;
+                count++;
+            } catch (e) {
+                return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
+            }
+            var rek = new RegExp("^(?:[^|\\)]*\\\)){" + count.toString() + "}([^|]*)", "gm");
+            var keepo = rek.exec(tmpP);
+            var pogchamp = keepo[0];
+            var vislaud = "";
+            keepo.shift();
+            for (var i = 0; i < keepo.length; i++) {
+                vislaud += keepo[i];
+            }
+            tmpP = "(" + tmpP.replace(vislaud, "");
+            var resultE = false;
+            var resultP;
+            try {
+                resultP = mathjs.eval(tmpP);
+                if (!(resultP == "Infinity" || resultP == "-Infinity" || resultP.toString() == "NaN")) {
+                    resultE = true;
+                }
+            } catch (e) {
+
+            }
+            if (!resultE) {
+                return sendMessage(channel, `${user.username}, can't check if it's a prime number WutFace`);
+            } else {
+                tmp = "isPrime(" + resultP + ")";
+            }
+        }
+    }
+    try {
+        var result = mathjs.eval(tmp);
+        if (result == "Infinity" || result == "-Infinity") {
+            result = `WutFace it's '${result}'`;
+        } else if (result.toString() == "NaN") {
+            result = `NaM it's '${result}'`;
+        }
+        if (result.toString().indexOf('function') > -1) {
+            result = `looks like you didn't close the brackets for a function :)`;
+        }
+        return sendMessage(channel, `${user.username}, ${result}`);
+    } catch (e) {
+        return sendMessage(channel, `${user.username}, invalid input OMGScoots`);
+    }
 }
 
 function hug(channel, user, message, args) {
-  if (args[0]) {
-    if (/^[a-zA-Z0-9_]{1,25}$/.test(args[0])) {
-      return sendMessage(channel, `${user.username} hugs ${args[0]} <3`);
+    if (args[0]) {
+        if (/^[a-zA-Z0-9_]{1,25}$/.test(args[0])) {
+            return sendMessage(channel, `${user.username} hugs ${args[0]} <3`);
+        } else {
+            return sendMessage(channel, `${user.username}, that is not a valid username OMGScoots`);
+        }
     } else {
-      return sendMessage(channel, `${user.username}, that is not a valid username OMGScoots`);
+        return;
     }
-  } else {
-    return;
-  }
 }
 
 function myEval(channel, user, message, args) {
-  if (user.admin) {
-    try {
-      return sendMessage(channel, `${eval(args.join(" "))}`);
-    } catch (e) {
-      return sendMessage(channel, e);
+    if (user.admin) {
+        try {
+            return sendMessage(channel, `${eval(args.join(" "))}`);
+        } catch (e) {
+            return sendMessage(channel, e);
+        }
     }
-  }
 }
 
 function version(channel, user, message, args) {
-  var keepo = require('./package.json');
-  return sendMessage(channel, `${keepo.name} running on v${keepo.version} for ${lib.msToTimeSting(Date.now() - startTime)}`);
+    var keepo = require('./package.json');
+    return sendMessage(channel, `${keepo.name} running on v${keepo.version} for ${lib.msToTimeSting(Date.now() - startTime)}`);
 }
