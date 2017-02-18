f8ae426ade8769344467d31f57dbb0a7d558ae7a
Ian321 <ignaz.k@live.de>
2017-02-18 13:26:27 +0100

    Fixed the getUser functions

--- lib.js
+++ lib.js
@@ -2,20 +2,21 @@
   Custom library
 */
 const got = require('got');
+const conf = require('./config.json');
 
 module.exports = {
-  msToTimeSting: function (ms) {
-    // This functions converts ms to a human friendly string.
-    var sec = Math.floor(ms / 1000);
-    var days = Math.floor(sec / 86400);
-		var hours= Math.floor(sec / 3600 % 60 % 60 % 24);
+	msToTimeSting: function (ms) {
+		// This functions converts ms to a human friendly string.
+		var sec = Math.floor(ms / 1000);
+		var days = Math.floor(sec / 86400);
+		var hours = Math.floor(sec / 3600 % 60 % 60 % 24);
 		var mins = Math.floor(sec / 60 % 60);
 		var secs = Math.floor(sec % 60);
 		var time = "";
 		if (days >= 2) {
-			time += days+" days";
+			time += days + " days";
 		} else if (days >= 1) {
-			time += days+" day";
+			time += days + " day";
 		}
 		if (days >= 1 && ((hours >= 1 && mins >= 1) || (hours >= 1 && secs >= 1) || (mins >= 1 && secs >= 1))) {
 			time += ", ";
@@ -23,9 +24,9 @@ module.exports = {
 			time += " and ";
 		}
 		if (hours >= 2) {
-			time += hours+" hours";
+			time += hours + " hours";
 		} else if (hours >= 1) {
-			time += hours+" hour";
+			time += hours + " hour";
 		}
 		if (hours >= 1 && mins >= 1 && secs >= 1) {
 			time += ", ";
@@ -33,53 +34,55 @@ module.exports = {
 			time += " and ";
 		}
 		if (mins >= 2) {
-			time += mins+" minutes";
+			time += mins + " minutes";
 		} else if (mins >= 1) {
-			time += mins+" minute";
+			time += mins + " minute";
 		}
 		if (mins >= 1 && secs >= 1) {
 			time += " and ";
 		}
 		if (secs >= 2) {
-			time += secs+" seconds";
+			time += secs + " seconds";
 		} else if (secs >= 1) {
-			time += secs+" second";
+			time += secs + " second";
 		}
 		return time;
-  },
-  stringToHex: function (str) {
-    var out = "";
-    for (var i = 0, len = str.length; i < len; i++) {
-      out = out+"\\x"+str.charCodeAt(i).toString(16);
-    }
-    return out;
-  },
+	},
+	stringToHex: function (str) {
+		var out = "";
+		for (var i = 0, len = str.length; i < len; i++) {
+			out = out + "\\x" + str.charCodeAt(i).toString(16);
+		}
+		return out;
+	},
 	getUser: {
-			id: function(name) {
-					return got.get(`https://api.twitch.tv/kraken/users?login=${name}`, {
-							json: true,
-							headers: {
-									Accept: "application/vnd.twitchtv.v5+json"
-							}
-					}).then(res => {
-							return res.body.users[0]._id;
-					}).catch(err => {
-							console.error(`Could not get id for '${name}'`);
-							return err;
-					});
-			},
-			data: function(id) {
-					return got.get(`https://api.twitch.tv/kraken/channels/${id}`, {
-							json: true,
-							headers: {
-									Accept: "application/vnd.twitchtv.v5+json"
-							}
-					}).then(res => {
-							return res.body;
-					}).catch(err => {
-							console.error(`Could not get date for '${id}'`);
-							return err;
-					});
-			}
+		id: function (name) {
+			return got.get(`https://api.twitch.tv/kraken/users?login=${name}`, {
+				json: true,
+				headers: {
+					Accept: "application/vnd.twitchtv.v5+json",
+					"client-id": conf.clientID || ""
+				}
+			}).then(res => {
+				return res.body.users[0]._id;
+			}).catch(err => {
+				console.error(`Could not get id for '${name}'`);
+				return err;
+			});
+		},
+		data: function (id) {
+			return got.get(`https://api.twitch.tv/kraken/channels/${id}`, {
+				json: true,
+				headers: {
+					Accept: "application/vnd.twitchtv.v5+json",
+					"client-id": conf.clientID || ""
+				}
+			}).then(res => {
+				return res.body;
+			}).catch(err => {
+				console.error(`Could not get date for '${id}'`);
+				return err;
+			});
+		}
 	}
 };
