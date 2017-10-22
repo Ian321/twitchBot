6a6931c800d71b8e0f5e9211c822c13a36b0715a
Ian321 <ignaz.k@live.de>
2017-10-22 15:07:31 +0200

    Updated test

--- test.js
+++ test.js
@@ -1,63 +1,64 @@
-var chai = require('chai');
-var tmi = require('tmi.js');
-var lib = require('./lib');
-var mathjs = require('mathjs');
-var expect = chai.expect;
+const chai = require('chai');
+const tmi = require('tmi.js');
+const lib = require('./lib');
+const mathjs = require('mathjs');
+
+const { expect } = chai;
 
 const options = {
-    options: {
-        debug: false
-    },
-    connection: {
-        reconnect: true
-    },
-    channels: ["#" + process.argv[2]]
+  options: {
+    debug: false
+  },
+  connection: {
+    reconnect: true
+  },
+  channels: ['#ian678']
 };
 const client = tmi.client(options);
 
-describe('Library', function() {
-  it('lib.stringToHex("Kappa 123") should return "4b6170706120313233"', function() {
-    expect(lib.stringToHex("Kappa 123").replace(/\\x/g, "")).to.equal("4b6170706120313233");
+describe('Library', () => {
+  it('lib.stringToHex("Kappa 123") should return "4b6170706120313233"', () => {
+    expect(lib.stringToHex('Kappa 123').replace(/\\x/g, '')).to.equal('4b6170706120313233');
   });
-  it('lib.msToTimeString(<1000) should return ""', function() {
-    expect(lib.msToTimeString(1)).to.equal("");
-    expect(lib.msToTimeString(500)).to.equal("");
-    expect(lib.msToTimeString(999)).to.equal("");
+  it('lib.msToTimeString(<1000) should return ""', () => {
+    expect(lib.msToTimeString(1)).to.equal('');
+    expect(lib.msToTimeString(500)).to.equal('');
+    expect(lib.msToTimeString(999)).to.equal('');
   });
-  it('lib.msToTimeString(1000) should return "1 second"', function() {
-    expect(lib.msToTimeString(1000)).to.equal("1 second");
+  it('lib.msToTimeString(1000) should return "1 second"', () => {
+    expect(lib.msToTimeString(1000)).to.equal('1 second');
   });
 });
 
-describe('TMI.js', function() {
-  it('Test connect', function() {
-    client.connect().catch(function(err) {
+describe('TMI.js', () => {
+  it('Test connect', () => {
+    client.connect().catch(err => {
       expect(err).to.equal(undefined);
     });
     client.disconnect();
   });
-  it('Test disconnect', function() {
+  it('Test disconnect', () => {
     client.connect();
-    client.disconnect().catch(function(err) {
+    client.disconnect().catch(err => {
       expect(err).to.equal(undefined);
     });
   });
 });
 
-describe('MATH', function() {
-  describe('mathjs', function() {
-    it('1+1^2-4 === -2', function() {
+describe('MATH', () => {
+  describe('mathjs', () => {
+    it('1+1**2-4 === -2', () => {
       try {
-        expect(mathjs.eval("1+1^2-4")).to.equal(-2);
+        expect(mathjs.eval('1+1^2-4')).to.equal(-2);
       } catch (err) {
         expect(err).to.equal(undefined);
       }
     });
   });
-  describe('node', function() {
-    it('1+1^2-4 === -2', function() {
+  describe('node', () => {
+    it('1+1**2-4 === -2', () => {
       try {
-        expect(eval("1+Math.pow(1,2)-4")).to.equal(-2);
+        expect((1 + (1 ** 2)) - 4).to.equal(-2);
       } catch (err) {
         expect(err).to.equal(undefined);
       }
@@ -65,19 +66,17 @@ describe('MATH', function() {
   });
 });
 
-describe('Twitch API', function() {
-  describe('Get ID by username', function() {
-    it('nuuls => 100229878', function () {
-      lib.getUser.id("nuuls").then(id => {
-          expect(id).to.eql("100229878");
-      });
+describe('Twitch API', () => {
+  describe('Get ID by username', () => {
+    it('nuuls => 100229878', async () => {
+      const id = await lib.getUser.id('nuuls');
+      expect(id).to.eql('100229878');
     });
   });
-  describe('Get data by id', function() {
-    it('100229878 => nuuls', function () {
-      lib.getUser.data("100229878").then(data => {
-          expect(data.created_at).to.eql("2015-08-22T13:43:40Z");
-      });
+  describe('Get data by id', () => {
+    it('100229878 => nuuls', async () => {
+      const data = lib.getUser.data('100229878');
+      expect(data.created_at).to.eql('2015-08-22T13:43:40Z');
     });
   });
 });
